4c71212931995548fbee39d328e94c3e0edc9430
mereka-vpu <80886830+mereka-vpu@users.noreply.github.com>
2025-04-12 13:57:14 +0700

    Update libcapi.c

--- libcapi.c
+++ libcapi.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <dlfcn.h>
-#include <sys/wait.h>
+#include <time.h>
+#include <stdlib.h>
 #include "libcapi.h"
 
 #define MAX_VARS 100
@@ -48,53 +49,53 @@ static const char *get_variable(const char *name) {
     return "";
 }
 
-static void run_function(const char *name, int client_socket) {
+static void append_output(CapiResult *res, const char *data) {
+    if (strlen(res->output) + strlen(data) < MAX_OUTPUT) {
+        strcat(res->output, data);
+    }
+}
+
+static void run_function(const char *name, CapiResult *res) {
     for (int i = 0; i < func_count; i++) {
         if (strcmp(funcs[i].name, name) == 0) {
             for (int j = 0; j < funcs[i].line_count; j++) {
-                char *line = funcs[i].lines[j];
-                if (strstr(line, "echo:")) {
-                    char var[64];
-                    sscanf(line, "echo: %[^;];", var);
-                    const char *val = get_variable(var);
-                    write(client_socket, val, strlen(val));
-                }
+                process_line(funcs[i].lines[j], res);
+                if (res->force_close) return;
             }
         }
     }
 }
 
-static void execute_shell(const char *cmd, int client_socket) {
+static void execute_shell(const char *cmd, CapiResult *res) {
     FILE *fp = popen(cmd, "r");
     if (!fp) return;
     char out[256];
     while (fgets(out, sizeof(out), fp)) {
-        write(client_socket, out, strlen(out));
+        append_output(res, out);
     }
     pclose(fp);
 }
 
-static void process_line(const char *line, int client_socket, CapiResponse *resp);
-
-static void include_file(const char *path, int client_socket, CapiResponse *resp) {
+static void include_file(const char *path, CapiResult *res) {
     FILE *file = fopen(path, "r");
     if (!file) return;
     char line[512];
     while (fgets(line, sizeof(line), file)) {
-        process_line(line, client_socket, resp);
+        process_line(line, res);
+        if (res->force_close) break;
     }
     fclose(file);
 }
 
-static void load_module(const char *mod_path) {
-    void *handle = dlopen(mod_path, RTLD_LAZY);
-    if (!handle) return;
-
-    void (*mod_init)() = dlsym(handle, "capi_mod_init");
-    if (mod_init) mod_init();
+void report_error(const char *msg, int line, const char *file) {
+    fprintf(stderr, "Error at line %d in %s: %s\n", line, file, msg);
 }
 
-static void process_line(const char *line, int client_socket, CapiResponse *resp) {
+void report_warning(const char *msg) {
+    fprintf(stderr, "Warning: %s\n", msg);
+}
+
+void process_line(const char *line, CapiResult *res) {
     if (strncmp(line, "#", 1) == 0) return;
 
     if (strstr(line, "int ") == line) {
@@ -105,41 +106,30 @@ static void process_line(const char *line, int client_socket, CapiResponse *resp
         char var[64];
         sscanf(line, "echo: %[^;];", var);
         const char *val = get_variable(var);
-        write(client_socket, val, strlen(val));
-    } else if (strstr(line, "header:") == line) {
-        char hdr[256];
-        sscanf(line, "header: \"%[^\"]", hdr);
-        if (resp->header_count < MAX_HEADERS) {
-            strcpy(resp->headers[resp->header_count++], hdr);
-        }
-    } else if (strstr(line, "http.code:") == line) {
-        sscanf(line, "http.code: %d;", &resp->status_code);
+        append_output(res, val);
+        append_output(res, "\n");
     } else if (strstr(line, "capi.forceclose()") != NULL) {
-        resp->force_close = 1;
+        report_warning("capi.forceclose() Deprecated");
     } else if (strstr(line, "capi.exec:") == line) {
         char cmd[256];
         sscanf(line, "capi.exec: \"%[^\"]", cmd);
-        execute_shell(cmd, client_socket);
-    } else if (strstr(line, "capi.loadmod(") == line) {
-        char mod[256];
-        sscanf(line, "capi.loadmod(\"%[^\"]", mod);
-        load_module(mod);
+        execute_shell(cmd, res);
     } else if (strstr(line, "incl <") == line) {
         char incl_path[256];
         sscanf(line, "incl <%[^>]", incl_path);
-        include_file(incl_path, client_socket, resp);
+        include_file(incl_path, res);
     } else if (strstr(line, "function ") == line) {
         CapiFunction *f = &funcs[func_count++];
         sscanf(line, "function %[^:]:", f->name);
         f->line_count = 0;
     } else if (strstr(line, "funclose;") == line) {
-        // Nothing to do
+        // End of function definition
     } else if (strchr(line, '(') && strchr(line, ')')) {
         char fname[64];
         sscanf(line, "%[^()]", fname);
-        run_function(fname, client_socket);
+        run_function(fname, res);
     } else {
-        // Inside function body
+        // Inside a function
         if (func_count > 0) {
             CapiFunction *f = &funcs[func_count - 1];
             if (f->line_count < MAX_LINES)
@@ -148,15 +138,17 @@ static void process_line(const char *line, int client_socket, CapiResponse *resp
     }
 }
 
-void process_capi_file(const char *filepath, int client_socket, CapiResponse *resp) {
+CapiResult process_capi_file(const char *filepath) {
+    CapiResult res = { .output = "", .force_close = 0 };
     FILE *file = fopen(filepath, "r");
-    if (!file) return;
+    if (!file) return res;
 
     char line[512];
     while (fgets(line, sizeof(line), file)) {
-        process_line(line, client_socket, resp);
-        if (resp->force_close) break;
+        process_line(line, &res);
+        if (res.force_close) break;
     }
 
     fclose(file);
+    return res;
 }
